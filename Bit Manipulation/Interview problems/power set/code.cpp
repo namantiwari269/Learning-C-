3f47ed2fffea264c6b93b31f67ad3e5b2afc49f6
namantiwari269 <naman_tiwari@icloud.com>
2025-08-28 21:18:23 +0530

    Bit Manipulation

---
+++ Bit Manipulation/Interview problems/power set/code.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    int n = nums.size();
+    int subsets= 1<<n;
+    vector<vector<int>> ans;
+    for(int i=0;i<subsets;i++){
+        vector<int> arr;
+        for(int j=0;j<n;j++){
+            if(i &(1<<j)){
+                arr.push_back(nums[j]);
+            }
+        
+        }
+    ans.push_back(arr);
+    }
+return ans;
+       
+}
